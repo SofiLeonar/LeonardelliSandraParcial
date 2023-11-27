37ad9a611f1e7a95d6926aea737ba577ccff84d0
SofiLeonar <143642462+SofiLeonar@users.noreply.github.com>
2023-11-27 14:50:44 -0300

    crear clase Circle

--- Shape.java
+++ Shape.java
@@ -1,3 +1,9 @@
  public abstract class Shape {
+     private String color;
+     private boolean filled;
 
+     public Shape() {
+         this.color = "red";
+         this.filled = true;
+     }
     }
