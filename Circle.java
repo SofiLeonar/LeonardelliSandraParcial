22899a12937d745270ef4b2552394363e05c4c62
SofiLeonar <143642462+SofiLeonar@users.noreply.github.com>
2023-11-27 14:53:02 -0300

    crear clase Rectangle

--- Circle.java
+++ Circle.java
@@ -1,2 +1,7 @@
-public class Circle {
-}
+ public class Circle extends Shape{
+        private double radius;
+        public Circle(){
+            super();
+            this.radius= 1.0
+        }
+    }
