2ba2b7d75a894fd3da341a5ba609257c5853462a
SofiLeonar <143642462+SofiLeonar@users.noreply.github.com>
2023-11-27 15:07:43 -0300

    crear getters y setters para width y length

--- Rectangle.java
+++ Rectangle.java
@@ -1,2 +1,27 @@
 public class Rectangle {
+public class Rectangle extends Shape{
+    private double width;
+    private double length;
+
+    public Rectangle(){
+        super();
+        this.width = 1.0;
+        this.length = 1.0;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
 }
