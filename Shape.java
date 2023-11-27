1ba1c8fd6d8e163b584efb312348bf9302ce0860
SofiLeonar <143642462+SofiLeonar@users.noreply.github.com>
2023-11-27 15:15:13 -0300

    crear getters y setters para color y filled

--- Shape.java
+++ Shape.java
@@ -6,4 +6,20 @@
          this.color = "red";
          this.filled = true;
      }
-    }
+
+     public String getColor() {
+         return color;
+     }
+
+     public void setColor(String color) {
+         this.color = color;
+     }
+
+     public boolean isFilled() {
+         return filled;
+     }
+
+     public void setFilled(boolean filled) {
+         this.filled = filled;
+     }
+ }
